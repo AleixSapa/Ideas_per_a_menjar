177aedd50abeed8fcb02a9e9868a85e118dac3cf
AleixSaPa <sapafamilia@gmail.com>
2025-01-11 21:21:25 +0100

    inci

---
+++ script.js
@@ -0,0 +1,14 @@
+mostrar={}
+Si="Si"
+function inici() {
+    ai_tomate=prompt("Tienes tomate?")
+    if(ai_tomate=Si){
+        mostrar=+["Amanida de tomaquet"] 
+    }
+}
+
+
+
+
+alert("Les ideas son:")
+alert(mostrar)
