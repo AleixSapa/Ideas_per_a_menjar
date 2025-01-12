5585267d36055042421cfef32d45c72269938b42
AleixSaPa <sapafamilia@gmail.com>
2025-01-12 21:41:53 +0100

    Script

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-mostrar={}
-function inici() {
-    ai_tomate=prompt("Tens tomate?")
-    if(ai_tomate="Si"){
-        mostrar=["Amanida de tomaquet"] 
+var mostrar={}
+var ai_tomate
+var i_ha_macarons
+function Els_ifs() {
+    ai_tomate=prompt("Tens tomaquet")
+    if(ai_tomate=="Si"){
+        mostrar=["Amenida de tomaquet"] 
     }
+    i_ha_macarons=prompt("Tens macarons")
+    if(i_ha_macarons=="Si"){
+        mostrar=["Macarons"] 
+    }
+    if(i_ha_macarons=="Si" && ai_tomate=="Si"){
+        mostrar=["Macarons amb tomaquet"]
+    }
+ 
+}
+function inici() {
+
+    Els_ifs()
     alert("Les ideas son:")
-    alert(mostrar)
-    
+    numero_contador=0
+        alert(mostrar[numero_contador])
 }
