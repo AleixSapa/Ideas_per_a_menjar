09b18f9ddaf1861670e68acbba1ac6d2cf9fccf6
AleixSaPa <sapafamilia@gmail.com>
2025-01-12 13:04:13 +0100

    No se

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 mostrar={}
-Si="Si"
 function inici() {
     ai_tomate=prompt("Tienes tomate?")
-    if(ai_tomate=Si){
-        mostrar=+["Amanida de tomaquet"] 
+    if(ai_tomate="Si"){
+        mostrar=["Amanida de tomaquet"] 
     }
+    
 }
-
-
-
-
-alert("Les ideas son:")
-alert(mostrar)
+function Fi(){
+    alert("Les ideas son:")
+    alert(mostrar)
+}
