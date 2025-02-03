467406a6ab332e5fe525de1fa33a495f574ed79a
AleixSaPa <sapafamilia@gmail.com>
2025-02-03 13:30:26 +0100

    chatgtp

--- script.js
+++ script.js
@@ -1,36 +1,8 @@
-var mostrar={}
-var ai_tomate
-var i_ha_macarons
-numero_contador=0
-function Preguntas() {
-    ai_tomate=prompt("Tens tomaquet")
-    i_ha_macarons=prompt("Tens macarons")
-}
-function Els_ifs() {
-    if(ai_tomate=="Si"){
-        mostrar=["Amenida de tomaquet"] 
-    }
-    if(i_ha_macarons=="Si"){
-        mostrar=["Macarons"] 
-    }
-    Convinats()
-}
-function Convinats() {
-    if(i_ha_macarons=="Si" && ai_tomate=="Si"){
-        mostrar=["Macarons amb tomaquet"]
-    } 
-}
-function inici() {
-    Preguntas(i_ha_macarons=prompt("Tens macarons"))
-    Els_ifs()
-    alert("Les ideas son:")
-    Fi()
-    Fi()
-    Fi()
-    Fi()
-    Fi()
-}
-function Fi() {
-    alert(mostrar[numero_contador])
-    numero_contador=numero_contador+1
-}
+document.addEventListener("DOMContentLoaded", () => {
+  const notification = document.getElementById("notification");
+  notification.classList.add("show");
+
+  setTimeout(() => {
+    notification.classList.remove("show");
+  }, 3000); // La notificació desapareix després de 3 segons
+});
